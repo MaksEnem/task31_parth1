29ed475ec61ee2351a450578d2d628a2e16f35cc
MaksEnem <110655471+MaksEnem@users.noreply.github.com>
2025-04-18 21:29:34 +1100

    preliminary working version

--- task1.cpp
+++ task1.cpp
@@ -16,7 +16,7 @@ public:
 	}
 
 	~Toy() {
-		std::cout << "Toy " << name << " was dropped " << std::endl;
+		//std::cout << "Toy " << name << " was dropped " << std::endl;
 	}
 };
 
@@ -35,6 +35,8 @@ public:
 		age = inAge;
 	}*/
 
+	Dog() {};
+
 	Dog(std::string inName, int inAge) : name(inName)
 	{
 		if (inAge > 0 && inAge < 30)
@@ -45,48 +47,73 @@ public:
 
 	void getToy(const std::shared_ptr<Toy>& inToy)
 	{
-		if (lovelyToy == nullptr)
+		
+		if (lovelyToy == nullptr && (inToy.use_count() - 1 == 0))
 		{			
-			lovelyToy = inToy;					
+			lovelyToy = inToy;		
+			std::cout << name << ": The toy has been picked up." << std::endl;
 		}
 		else if (lovelyToy != nullptr)
 		{
-			std::cout << "I already have this toy." << std::endl;
+			std::cout << name << ": I already have this toy." << std::endl;
 		}
+		else
+		{
+			std::cout << name << ": Another dog is playing with this toy." << std::endl;
+		}		
 	}
 
 
-	void count(std::shared_ptr<Toy> inToy)
+
+
+	void dropToy()
 	{
-		std::cout << "in fun(): sp.use_count() == " << inToy.use_count()
-			<< " (object @ " << inToy << ")\n";
+		if (lovelyToy != 0)
+		{
+			lovelyToy.reset();	
+			std::cout << name << ": threw the toy" << std::endl;
+		}
+		else
+		{
+			std::cout << name << ": Nothing to drop." << std::endl;
+		}
+		
 	}
 
-
-
-
-
-
-
-	std::string getName()
-	{
-		return name;
-	}
-	int getAge()
-	{
-		return age;
-	}
 
 
 	std::string getToyName()
 	{
 		return lovelyToy->getToyName();
+		
 	}
 
 	
+	std::string getDogName()
+	{
+		return name;
+	}
+	int getDogAge()
+	{
+		return age;
+	}
 
-	
 
+	void setName()
+	{
+		std::cout << "Enter dog's name: ";
+		std::cin >> name;
+	}
+	void setAge()
+	{
+		std::cout << "Enter dog's age: ";
+		std::cin >> age;
+	}
+
+	~Dog()
+	{
+		
+	}
 };
 
 
@@ -102,19 +129,67 @@ int main()
 	std::cout << "a: " <<  a->getToyName() << std::endl;*/
 	
 
+
+
+
+
+	//std::shared_ptr<Toy> toy = std::make_shared<Toy>("SomeToy");
+	//std::cout << "Name of the toy: " << toy->getToyName() << std::endl;
+	//
+	//
+	//std::shared_ptr<Dog> dog = std::make_shared<Dog>("Sharik", 10);
+	//
+	//
+	////std::cout<< "Dog name and age: " << dog->getName() << " " << dog->getAge() << std::endl;
+	//dog->getToy(toy);	
+	//dog->dropToy();
+	//dog->count(toy);
+	
+
+	
+
+
+
+
+
+	//std::shared_ptr<Toy> toy = std::make_shared<Toy>("SomeToy");
+	////std::cout << toy.use_count() << std::endl;
+
+	//std::shared_ptr<Dog> a = std::make_shared<Dog>("Sharik", 10);
+	//std::shared_ptr<Dog> b = std::make_shared<Dog>("Bobik", 12);
+	//std::shared_ptr<Dog> c = std::make_shared<Dog>("Druzhok", 11);
+
+
+
+	//a->getToy(toy);		
+	//b->getToy(toy);
+	//
+	////b->getToy(toy);
+	//
+	//a->dropToy();
+
+	//b->getToy(toy);
+	
+	
+
 	std::shared_ptr<Toy> toy = std::make_shared<Toy>("SomeToy");
-	std::cout << "Name of the toy: " << toy->getToyName() << std::endl;
+
 	
+	Dog* dog[3];
+	dog[0] = new Dog("Sharik", 10);
+	dog[1] = new Dog("Bobik", 12);
+	dog[2] = new Dog("Druzhok", 11);
+
 	
-	std::shared_ptr<Dog> dog = std::make_shared<Dog>("Sharik", 10);
-	std::shared_ptr<Dog> dog2 = std::make_shared<Dog>("Sharik", 10);
+	dog[0]->getToy(toy);
+	dog[1]->getToy(toy);
 	
-	//std::cout<< "Dog name and age: " << dog->getName() << " " << dog->getAge() << std::endl;
-	dog->getToy(toy);
-	dog2->getToy(toy);
+	//b->getToy(toy);
 	
-	
-	dog->count(toy);
-	dog2->count(toy);
+	dog[0]->dropToy();
+
+	dog[1]->getToy(toy);
+
+
 	return 0;
 }
